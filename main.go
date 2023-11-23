bc908f203361dba35ad206ee942b834c1d9fcb87
Nazan <nazhard@proton.me>
2023-11-23 17:15:50 +0700

    chore(format): reformat code

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-	"fmt"
-	
-	"github.com/nazhard/url-shortener-api/handler"
-	"github.com/nazhard/url-shortener-api/store"
-	"github.com/gin-gonic/gin"
+  "fmt"
+  
+  "github.com/nazhard/url-shortener-api/handler"
+  "github.com/nazhard/url-shortener-api/store"
+  "github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-		  "message": "Welcome to the URL Shortener API",
-		  "create_url": "/shorten",
-		})
-	})
-
-	r.POST("/shorten", func(c *gin.Context) {
-		handler.CreateShortUrl(c)
-	})
-
-	r.GET("/:shortUrl", func(c *gin.Context) {
-		handler.HandleShortUrlRedirect(c)
-	})
-
-	// Note store initialization happens here
-	store.InitializeStore()
-
-	err := r.Run(":9808")
-	if err != nil {
-		panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
-	}
+  r := gin.Default()
+  r.GET("/", func(c *gin.Context) {
+    c.JSON(200, gin.H{
+      "message": "Welcome to the URL Shortener API",
+      "create_url": "/shorten",
+    })
+  })
+  
+  r.POST("/shorten", func(c *gin.Context) {
+    handler.CreateShortUrl(c)
+  })
+  
+  r.GET("/:shortUrl", func(c *gin.Context) {
+    handler.HandleShortUrlRedirect(c)
+  })
+  
+  // Note store initialization happens here
+  store.InitializeStore()
+  err := r.Run(":9808")
+  if err != nil {
+    panic(fmt.Sprintf("Failed to start the web server - Error: %v", err))
+  }
 }
--- store/store_service.go
+++ store/store_service.go
@@ -1,54 +1,53 @@
 package store
 
 import (
-	"context"
-	"fmt"
-	"time"
-	
-	"github.com/go-redis/redis/v8"
+  "context"
+  "fmt"
+  "time"
+  
+  "github.com/go-redis/redis/v8"
 )
 
 var (
-	storeService = &StorageService{}
-	ctx          = context.Background()
+  storeService = &StorageService{}
+  ctx = context.Background()
 )
 
 const CacheDuration = 6 * time.Hour
 
 type StorageService struct {
-	redisClient *redis.Client
+  redisClient *redis.Client
 }
 
 func InitializeStore() *StorageService {
-	redisClient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
-
-	pong, err := redisClient.Ping(ctx).Result()
-	if err != nil {
-		panic(fmt.Sprintf("Error init Redis: %v", err))
-	}
-
-	fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
-	storeService.redisClient = redisClient
-	return storeService
+  redisClient := redis.NewClient(&redis.Options{
+    Addr:     "localhost:6379",
+    Password: "",
+    DB:       0,
+  })
+  
+  pong, err := redisClient.Ping(ctx).Result()
+  if err != nil {
+    panic(fmt.Sprintf("Error init Redis: %v", err))
+  }
+  
+  fmt.Printf("\nRedis started successfully: pong message = {%s}", pong)
+  storeService.redisClient = redisClient
+  return storeService
 }
 
 func SaveUrlMapping(shortUrl string, originalUrl string, userId string) {
-	err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
-	if err != nil {
-		panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
-	}
-
-	fmt.Printf("Saved shortUrl: %s - originalUrl: %s\n", shortUrl, originalUrl)
+  err := storeService.redisClient.Set(ctx, shortUrl, originalUrl, CacheDuration).Err()
+  if err != nil {
+    panic(fmt.Sprintf("Failed saving key url | Error: %v - shortUrl: %s - originalUrl: %s\n", err, shortUrl, originalUrl))
+  }
+  fmt.Printf("Saved shortUrl: %s - originalUrl: %s\n", shortUrl, originalUrl)
 }
 
 func RetrieveInitialUrl(shortUrl string) string {
-	result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
-	if err != nil {
-		panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
-	}
-	return result
+  result, err := storeService.redisClient.Get(ctx, shortUrl).Result()
+  if err != nil {
+    panic(fmt.Sprintf("Failed RetrieveInitialUrl url | Error: %v - shortUrl: %s\n", err, shortUrl))
+  }
+  return result
 }
